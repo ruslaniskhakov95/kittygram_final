01cb2737c7bcc9522dfd77348eb0e067718a0534
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-06-24 15:55:20 +0300

    CI/CD setup, host allowed

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,10 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'iskhakovkittygram.ru',
+    '158.160.27.182', 'localhost'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
