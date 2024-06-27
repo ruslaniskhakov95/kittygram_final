adfc81576ba1f70289c4172d5cc47aa5deeb3e34
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-06-27 23:26:33 +0300

    Update DEBUG

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG').lower() == 'true'
 
 ALLOWED_HOSTS = [
     os.getenv('HOST_DOMAIN'),
